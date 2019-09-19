03659e53ebd4c1311d192e4fe2ceae50851e6e28
dhochbaum <dhochbaum@gmail.com>
2019-09-19 15:06:39 -0400

    gave single movie view some formatting

--- client/components/singleMovieView.js
+++ client/components/singleMovieView.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 import {getSingleMovieThunk} from '../store/movies'
 
 class DisconnectedSingleMovieView extends React.Component {
@@ -12,16 +13,63 @@ class DisconnectedSingleMovieView extends React.Component {
 
   render() {
     const movie = this.props.movies[0]
+    let lastTag
     if (!movie) {
       return <div>...Loading</div>
     }
+    lastTag = movie.tags.pop()
+
     return (
-      <div>
-        <h1>{movie.title}</h1>
-        <img src={movie.imageUrl} />
-        <p>{movie.description}</p>
-        <p>${movie.price}</p>
-        <button type="button"> Add to Cart</button>
+      <div className="container is-fluid">
+        <div className="tile is-ancestor">
+          <div className="tile is-6">
+            <img src={movie.imageUrl} />
+          </div>
+
+          <div className="container is-fluid">
+            <div className="tile is-vertical">
+              <div className="tile">
+                <h1>{movie.title}</h1>
+              </div>
+              <div className="tile">
+                <p>{movie.description}</p>
+              </div>
+              <div className="tile">
+                <p>${movie.price / 100}</p>
+              </div>
+              <div className="tile">
+                <form>
+                  Quantity:{' '}
+                  <input
+                    type="number"
+                    name="quantity"
+                    min="1"
+                    defaultValue="1"
+                    id="quantity-input"
+                  />{' '}
+                  &nbsp;
+                  <button type="submit">Add to Cart</button>
+                </form>
+              </div>
+              <div className="tile">
+                <p>
+                  {movie.tags.map(tag => {
+                    return (
+                      <Link key={tag} to={`/movies/tags/${tag}`}>
+                        {tag},&nbsp;
+                      </Link>
+                    )
+                  })}
+                  {
+                    <Link key={lastTag} to={`/movies/tags/${lastTag}`}>
+                      {lastTag}
+                    </Link>
+                  }
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
