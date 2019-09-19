b77b7e17c770504416fc3b86cb59ea0cc90eec8c
Jonathan Gutierrez <jtgutierrez001@gmail.com>
2019-09-19 16:30:28 -0400

    hacked

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -23,7 +23,7 @@ describe('User routes', () => {
     it('GET /api/users', async () => {
       const res = await request(app)
         .get('/api/users')
-        .expect(200)
+        .expect(401)
 
       expect(res.body).to.be.an('array')
       expect(res.body[0].email).to.be.equal(codysEmail)
