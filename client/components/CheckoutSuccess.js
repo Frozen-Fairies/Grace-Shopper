4efec2f75f97ee56a249ddc4427eaf9ced9e2856
Kenneth Lai <klnyc6@gmail.com>
2019-09-25 00:00:40 -0400

    minor css and text changes

--- client/components/CheckoutSuccess.js
+++ client/components/CheckoutSuccess.js
@@ -3,12 +3,14 @@ import {Link} from 'react-router-dom'
 
 const CheckoutSuccsess = () => {
   return (
-    <div>
-      <img src="https://i.ytimg.com/vi/DqgqnoESw3w/maxresdefault.jpg" />
-      <p>Thanks for shopping at Frozen Fairy Flix</p>
-      <Link to="/">
-        <button type="button">Back To Movies</button>
-      </Link>
+    <div className="columns is-centered">
+      <div className="column has-text-centered">
+        <img src="https://i.ytimg.com/vi/DqgqnoESw3w/maxresdefault.jpg" />
+        <p>Thanks for shopping at Frozen Fairy Flix</p>
+        <Link to="/">
+          <button type="button">Back To Movies</button>
+        </Link>
+      </div>
     </div>
   )
 }
